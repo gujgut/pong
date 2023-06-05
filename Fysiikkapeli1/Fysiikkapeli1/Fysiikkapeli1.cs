3d7393338987312e949ebedf497bdd85d6d4beaf
vaanvi <vilppuvaananen@gmail.com>
2023-06-05 14:52:41 +0300

    tein jotain

--- Fysiikkapeli1/Fysiikkapeli1/Fysiikkapeli1.cs
+++ Fysiikkapeli1/Fysiikkapeli1/Fysiikkapeli1.cs
@@ -9,11 +9,30 @@ namespace Fysiikkapeli1;
 
 public class Fysiikkapeli1 : PhysicsGame
 {
+    private PhysicsObject pallo;
+    
     public override void Begin()
     {
+
+        LuoKentta();
+        Vector impulssi = new Vector(500.0, 0.0);
+        pallo.Hit(impulssi);
+        
         // Kirjoita ohjelmakoodisi tähän
 
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+
+
+    void LuoKentta()
+    {
+         pallo = new PhysicsObject(40.0, 40.0);
+        Add(pallo);
+        pallo.Shape = Shape.Circle;
+        Level.CreateBorders(1.0, false);
+        pallo.Restitution = 1.0;
+        Level.Background.Color = Color.Black;
+        Camera.ZoomToLevel();
     }
 }
